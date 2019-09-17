1073351383373ffa43709ba7416481ce134d23d3
Don Isaac <donald.isaac@gmail.com>
2019-09-17 15:06:29 -0400

    Commented out guild while in development

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  namespace :guild do
-    get 'dashboard/index'
-    get 'dashboard/dash'
-    get 'dashboard/signin'
-    get 'dashboard/signup'
-    get 'dashboard/signout'
-  end
+  # namespace :guild do
+  #   get 'dashboard/index'
+  #   get 'dashboard/dash'
+  #   get 'dashboard/signin'
+  #   get 'dashboard/signup'
+  #   get 'dashboard/signout'
+  # end
   # Landing Pages
   root 'home#index'
   # get '/get-involved', to: 'home#get_involved'
