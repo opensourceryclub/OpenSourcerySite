16eed72b78154305771be3b134b494187d9c707d
Zach Wild <zacharyjwild@gmail.com>
2020-05-19 23:35:02 -0400

    Fix server ip in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 # add server to known hosts
-ssh-keyscan -H 172.31.40.141 >> ~/.ssh/known_hosts
+ssh-keyscan -H 13.58.120.127 >> ~/.ssh/known_hosts
 
 # connect to server
 ssh site@opensourceryumd.com
