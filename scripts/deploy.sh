34f7acdcc8b2ae2c121baa9606edae66e923c869
Zach Wild <zacharyjwild@gmail.com>
2020-05-21 23:22:18 -0400

    Update deploy script to run commands in SSH session correctly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,17 @@
 # Connect to web host
-ssh site@opensourceryumd.com
-cd ~/site
-
 # Stop web server daemon
-pm2 stop site
-
 # Build site with architekt
-npm install
-ark render
-
 # Start web server daemon
-pm2 start index.js --name site
+# Exit SSH session
+ssh site@opensourceryumd.com "
+cd ~/site &&
 
-# Close SSH connection
+pm2 stop site &&
+
+npm install &&
+ark render &&
+
+pm2 start index.js --name site &&
+
 exit
+"
